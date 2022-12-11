2c3977255d9c378eced7166e0a6e3bd61168d638
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2022-12-11 01:13:55 +0000

    dynamically add  css classes, implement local storage, time formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
+function getLocalStorage(key) {
+  let value = localStorage.getItem(key);
+  if (value) {
+    $(`#text${key}`).text(value);
+  }
+}
+
 $(document).ready(function () {
-  //   Display the current day at the top of the calender when a user opens the planner.
+  //  Display the current day at the top of the calender when a user opens the planner.
   $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 
-  //   Present timeblocks for standard business hours when the user scrolls down.
+  //   Present timeblocks for standard business hours (9am-6pm chosen) when the user scrolls down.
   for (let i = 9; i < 19; i++) {
     // create a row
     var row = $(`<div data-time=${i} id='${i}' class="row">`);
@@ -29,5 +36,41 @@ $(document).ready(function () {
 
     // last step add rows to container
     $(".container").append(row);
+
+    getLocalStorage(i);
   }
+
+  function formatAMPM(hours) {
+    var ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    return hours + ampm;
+  }
+  formatAMPM();
+
+  // add css class based on time that will color-code timeblocks based on past, present, and future
+
+  function updateColors() {
+    var currentTime = new Date().getHours();
+    for (var i = 9; i < 24; i++) {
+      console.log(currentTime, $(`#${i}`).data("time"));
+      if ($(`#${i}`).data("time") == currentTime) {
+        $(`#text${i}`).addClass("present");
+      } else if (currentTime < $(`#${i}`).data("time")) {
+        $(`#text${i}`).addClass("future");
+      }
+    }
+  }
+
+  setInterval(function () {
+    updateColors();
+  }, 1000);
+
+  // on click save to localStorage
+  var saveBtn = $(".saveBtn");
+  saveBtn.on("click", function () {
+    let eventId = $(this).attr("id");
+    let eventText = $(this).parent().siblings().children(".description").val();
+    localStorage.setItem(eventId, eventText);
+  });
 });
